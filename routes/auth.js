2c9fa26375b534d18ea0b6548e0e8ade076b0a09
Sam Smiley <samtsmiley@gmail.com>
2018-12-04 12:14:57 -0800

    made jwt.js in passport and added a createtoken funciton

--- routes/auth.js
+++ routes/auth.js
@@ -3,17 +3,27 @@
 const express = require('express');
 // const mongoose = require('mongoose');
 // const User = require('../models/user');
+const { JWT_SECRET, JWT_EXPIRY } = require('../config');
 const router = express.Router();
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 
+function createAuthToken (user) {
+  return jwt.sign({user}, JWT_SECRET, {
+    subject:user.username,
+    expiresIn: JWT_EXPIRY
+  });
+}
 
 const options = {session: false, failWithError: true};
 
 const localAuth = passport.authenticate('local', options);
 
 router.post('/login', localAuth, function (req, res) {
-  return res.json(req.user);
+  const authToken = createAuthToken(req.user);
+  res.json({authToken});
 });
+
 
 module.exports = router;
